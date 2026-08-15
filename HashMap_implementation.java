1d336421c8c899eba16ec446b1c44f04c08f8631
Deepal008 <yadavdeepal748@gmail.com>
2026-08-15 17:24:05 +0530

    HashMap part 1

---
+++ HashMap_implementation.java
@@ -0,0 +1,156 @@
+import java.util.*;
+
+public class HashMap_implementation {
+    static class HashMap<K, V> {  //generic
+        private class Node {
+            K key;
+            V value;
+
+            public Node(K key, V value){
+                this.key = key;
+                this.value = value;
+            }
+        }
+
+        private int n;  //n
+        private int N;
+        private ArrayList<Node>[] buckets;  //N  = buckets.length
+
+        @SuppressWarnings("unchecked")
+        public HashMap(){
+            this.N = 4;
+            this.buckets = new ArrayList[4];
+            for(int i=0; i<4; i++){
+                this.buckets[i] = new ArrayList<>();
+            }
+        }
+
+        private int hashFunction(K key){
+            int hc = key.hashCode();
+            return Math.abs(hc)  % N;
+        }
+
+        private int SearchInLL(K key, int bi){
+            ArrayList<Node> ll = buckets[bi];
+
+            for(int i=0; i<ll.size(); i++){
+                Node node = ll.get(i);
+                int di = 0;
+
+                if(node.key == key){
+                    return di;
+                }
+
+                di++;
+            }
+
+            return -1;
+        }
+
+        @SuppressWarnings("unchecked")
+        private void rehash(){
+            ArrayList<Node>[] oldBuck = buckets;
+            buckets = new ArrayList[N*2];
+            N = 2*N;
+
+            for(int i=0; i<buckets.length; i++){
+                buckets[i] = new ArrayList<>();
+            }
+
+            //nodes -> add i buckets 
+            for(int i=0; i< oldBuck.length; i++){
+                ArrayList<Node> ll = oldBuck[i];
+                for(int j=0; j<ll.size(); j++){
+                    Node node = ll.remove(0);
+                    put(node.key, node.value);
+                }
+            }
+        }
+
+        public void put(K key, V value){
+            int bi = hashFunction(key);   //0 to size-1
+            int di = SearchInLL(key, bi);    //valid; -1
+
+            if(di != -1){
+                Node node = buckets[bi].get(di);
+                node.value = value;
+            }else{
+                buckets[bi].add(new Node(key, value));
+                n++;
+            }
+
+            double lambda = (double)n/N;
+
+            if(lambda > 2.0){
+                rehash();
+            }
+        }
+        
+        
+        public boolean containsKey(K key){
+            int bi = hashFunction(key);   //0 to size-1
+            int di = SearchInLL(key, bi);    //valid; -1
+
+            if(di != -1){
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+        public V get(K key){
+             int bi = hashFunction(key);   //0 to size-1
+            int di = SearchInLL(key, bi);    //valid; -1
+
+            if(di != -1){
+                Node node = buckets[bi].get(di);
+                return node.value;
+            }else{
+                return null;
+            }
+        }
+
+        public V remove(K key){
+             int bi = hashFunction(key);   //0 to size-1
+            int di = SearchInLL(key, bi);    //valid; -1
+
+            if(di != -1){
+                Node node = buckets[bi].remove(di);
+                n--;
+
+                return node.value;
+            }else{
+                return null;
+            }
+        }
+
+        public ArrayList<K> keySet(){
+            ArrayList<K> keys = new ArrayList<>();
+
+            for(int i=0; i<buckets.length; i++){
+               ArrayList<Node> ll = buckets[i];
+               for(Node node : ll){
+                keys.add(node.key);
+               } 
+            }
+            return keys;
+        }
+
+        public boolean isEmpty(){
+            return n==0;
+        }
+    }
+    public static void main(String[] args){
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        hm.put("India", 100);
+        hm.put("China", 150);
+        hm.put("US", 50);
+        hm.put("Nepal", 5);
+
+        ArrayList<String>keys = hm.keySet();
+        for(String key : keys){
+            System.out.println(key); 
+        }
+    }
+}
---
+++ HashMap_majorityElement.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class HashMap_majorityElement {
+
+    public static void majorityElement(int[] nums){
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        
+
+        for(int i=0; i<nums.length; i++){
+
+            // if(hm.containsKey(nums[i])){
+            //     hm.put(nums[i], hm.get(nums[i])+1);
+            // }else{
+            //     hm.put(nums[i], 1);
+            // }
+
+            hm.put(nums[i], hm.getOrDefault(nums[i], 0) + 1); 
+        }
+
+        for(int key : hm.keySet()){
+            if(hm.get(key) > nums.length/3){
+                System.out.println(key);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,3,2,5,1,3,1,5,1};
+        majorityElement(nums);
+    }
+}
